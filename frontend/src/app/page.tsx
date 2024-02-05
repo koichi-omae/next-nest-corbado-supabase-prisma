a6850837281fe2f7b13df2ee6a024478fd5fa620
junk dog <ohmae@arubeh.com>
2024-02-05 12:48:59 +0900

    Topページの作成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,16 @@
-import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return <div className=' font-bold text-red-500'>test</div>;
+  return (
+    <div className='flex justify-center items-center h-screen bg-gradient-to-bl from-slate-300 to-slate-200 '>
+      <div className='text-center'>
+        <h1 className='text-6xl font-bold tracking-wide text-sky-600'>stak Onbording</h1>
+        <Link href='/auth'>
+          <button className='mt-8 text-xl font-semibold bg-gradient-to-l from-green-600 to-green-500 text-white rounded-md py-4 px-8 hover:opacity-75'>
+            ログイン
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
