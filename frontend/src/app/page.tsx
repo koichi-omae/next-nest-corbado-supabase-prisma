d4c87c9327b59f59b60e7169ba85d34d3008bd51
junk dog <ohmae@arubeh.com>
2024-02-05 16:31:56 +0900

    リンク先の変更

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className='flex justify-center items-center h-screen bg-gradient-to-bl from-slate-300 to-slate-200 '>
       <div className='text-center'>
         <h1 className='text-6xl font-bold tracking-wide text-sky-600'>stak Onbording</h1>
-        <Link href='/auth'>
+        <Link href='/login'>
           <button className='mt-8 text-xl font-semibold bg-gradient-to-l from-green-600 to-green-500 text-white rounded-md py-4 px-8 hover:opacity-75'>
             ログイン
           </button>
