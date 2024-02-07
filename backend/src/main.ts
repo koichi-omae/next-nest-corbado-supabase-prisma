92d2ebb5de7d606fdea770e551b1ada649ebf552
junk dog <ohmae@arubeh.com>
2024-02-07 23:25:24 +0900

    CROSの設定

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
   await app.listen(8080);
 }
 bootstrap();
